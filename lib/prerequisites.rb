2f28f72409c30cd81fc4a5eb1bebcbfbb1e01861
David Salgado <david@digitalronin.com>
2020-03-22 13:31:30 +0000

    Check for the presence of environment variables

--- lib/prerequisites.rb
+++ lib/prerequisites.rb
@@ -6,7 +6,19 @@ class Prerequisites
   end
 
   def check
+    environment_variables
+  end
+
+  private
+
+  def environment_variables
+    config.dig(:environment_variables).to_a.each do |env_var|
+      raise Prerequisites::EnvironmentVariableError.new("Required environment variable #{env_var} is not set") unless ENV.key?(env_var)
+    end
+
     true
   end
 
 end
+
+class Prerequisites::EnvironmentVariableError < RuntimeError; end
--- spec/prerequisites_spec.rb
+++ spec/prerequisites_spec.rb
@@ -1,7 +1,14 @@
 require "spec_helper"
 
-describe Prerequisites do
-  let(:config) { {} }
+describe Prerequisites, "environment variables" do
+  let(:environment_variables) { nil }
+
+  let(:config) {
+    {
+      environment_variables: environment_variables,
+    }
+  }
+
   subject(:prereq) { described_class.new(config) }
 
   it "instantiates" do
@@ -10,5 +17,27 @@ describe Prerequisites do
 
   it "checks" do
     expect(prereq.check).to be(true)
+  end
+
+  context "when a required env. var. is set" do
+    let(:environment_variables) { ["FOO"] }
+
+    before do
+      allow(ENV).to receive(:key?).with("FOO").and_return(true)
+    end
+
+    it "checks" do
+      expect(prereq.check).to be(true)
+    end
+  end
+
+  context "when a required env. var. is not set" do
+    let(:environment_variables) { ["FOO"] }
+
+    it "raises an error" do
+      expect {
+        prereq.check
+      }.to raise_error(Prerequisites::EnvironmentVariableError)
+    end
   end
 end
